0bb68044e7cfd03d2104fda24e96798db95df5b8
joselopezbinoovo <joselopez@binoovo.com>
2024-03-11 09:03:15 +0100

    traducciones hechas

--- src/app/presentation/pages/translatePage/translatePage.component.ts
+++ src/app/presentation/pages/translatePage/translatePage.component.ts
@@ -1,13 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
+import { MyMessageComponent } from '../../components/chat-bubbles/myMessage/myMessage.component';
+import { TypingLoaderComponent } from '../../components/typingLoader/typingLoader.component';
+import { TextMessageBoxComponent } from '../../components/text-boxes/textMessageBox/textMessageBox.component';
+import { TextMessageBoxFileComponent } from '../../components/text-boxes/textMessageBoxFile/textMessageBoxFile.component';
+import { TextMessageBoxEvent, TextMessageBoxSelectComponent } from '../../components/text-boxes/textMessageBoxSelect/textMessageBoxSelect.component';
+import { Message } from '../../../interfaces';
+import { OpenAiServie } from '../../services/openai.service';
 
 @Component({
   selector: 'app-translate-page',
   standalone: true,
   imports: [
     CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxComponent,
+    TextMessageBoxFileComponent,
+    TextMessageBoxSelectComponent,
   ],
   templateUrl: './translatePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TranslatePageComponent { }
+export default class TranslatePageComponent {
+
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+  public openAiService = inject(OpenAiServie);
+
+  public languages = signal( [
+    { id: 'alemán', text: 'Alemán' },
+    { id: 'árabe', text: 'Árabe' },
+    { id: 'bengalí', text: 'Bengalí' },
+    { id: 'francés', text: 'Francés' },
+    { id: 'hindi', text: 'Hindi' },
+    { id: 'inglés', text: 'Inglés' },
+    { id: 'japonés', text: 'Japonés' },
+    { id: 'mandarín', text: 'Mandarín' },
+    { id: 'portugués', text: 'Portugués' },
+    { id: 'ruso', text: 'Ruso' },
+  ]);
+
+
+
+  handleMessageWithSelect( {prompt, selectedOption}:TextMessageBoxEvent){
+
+    const message = `Traduce a ${selectedOption}: ${prompt}`;
+    this.isLoading.set(true);
+    this.messages.update(prev => [ ...prev, {text:message, isGpt:false}]);
+
+    this.openAiService.translateText(prompt,selectedOption).subscribe(({message}) => {
+      this.isLoading.set(false);
+      this.messages.update(prev => [...prev, {text:message, isGpt:true}])
+    })
+    }
+}
